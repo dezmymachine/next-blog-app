350488f314fcb6ff3f2079e751344f4485eaabb3
dezmymachine <desmondkwakucoffie195@gmail.com>
2024-06-28 20:58:24 +0000

    setup and initialisation

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="">
+      <p>I am a blog site and I will happen soon !</p>
+    </main>
+  );
+}
