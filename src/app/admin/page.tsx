fee68e673a697f7f2f4221aa49f10b68653b932b
dezmymachine <desmondkwakucoffie195@gmail.com>
2024-07-02 16:38:20 +0000

    added type to post fetch admin

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -2,20 +2,24 @@ import Link from "next/link";
 import React from "react";
 import { prisma } from "@/lib/db";
 
-
+type Post = {
+  id: string;
+  title: string;
+};
 export default async function page() {
-  const posts = await prisma.post.findMany();
+  const posts: Post[] = await prisma.post.findMany();
   return (
     <div className="min-h-screen py-20 px-10">
       <h3 className="">All Posts</h3>
       {posts &&
-        posts.map((post) => (
+        posts.map((post: Post, index: number) => (
           <div key={post.id} className="flex items-center gap-x-2">
+            <p>{index + 1}</p>
             <p className="font-bold">{post.title}</p>
           </div>
         ))}
       <div className="mt-4">
-        <Link href="/admin/create" className="border border-orange-300 p-2">
+        <Link href="/admin/create" className="text-white bg-orange-500 p-2">
           Create Post
         </Link>
       </div>
